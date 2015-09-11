5d2428e06e027f76489d63faf69881c733c0e2aa
unknown <logaraja.m@gmail.com>
2015-09-11 17:08:15 +0530

    Added Window's Chrome Driver - Logu

--- src/main/java/main/java/MyStepdefs.java
+++ src/main/java/main/java/MyStepdefs.java
@@ -2,7 +2,6 @@ package main.java;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by sramalin on 07/09/15.
@@ -11,7 +10,8 @@ public class MyStepdefs {
     @cucumber.api.java.en.Given("^I launch the app$")
     public void I_launch_the_app() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        System.setProperty("webdriver.chrome.driver", "/Users/sramalin/Documents/Personal/HospitalManagement/HospitalTestAutomation/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "../hospitaltestautomation/src/chromedriver/chromedriverMAC");
+        System.setProperty("webdriver.chrome.driver", "../hospitaltestautomation/src/chromedriver/chromedriver_win32/chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
         webDriver.navigate().to("http://localhost:8000/aspire");
     }
